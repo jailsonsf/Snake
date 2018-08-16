286a1026de58ec75ae23786b904b0d319b9d8f08
jailsonsf <jailsonsoares002@gmail.com>
2018-08-16 08:53:18 -0300

    Snake Game

---
+++ game_functions.py
@@ -0,0 +1,28 @@
+import pygame
+
+pygame.font.init()
+
+largura = 360
+altura = 480
+tamanho = 10
+
+branco = (255,255,255)
+preto = (0,0,0)
+vermelho = (255,0,0)
+verde = (0,255,0)
+azul = (0,0,255)
+
+fundo = pygame.display.set_mode((largura, altura))
+font = pygame.font.SysFont(None, 18)
+
+
+def text(msg, cor):
+    texto = font.render(msg, True, cor)
+    fundo.blit(texto, [largura/10, altura/5])
+
+def snake(cobra_xy):
+    for xy in cobra_xy:
+        pygame.draw.rect(fundo, branco, [xy[0], xy[1], tamanho, tamanho])
+
+def fruit(pos_x, pos_y):
+    pygame.draw.rect(fundo, vermelho, [pos_x, pos_y, tamanho, tamanho])
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pygame
+from game_functions import text, snake, fruit
 from random import randrange
 
 pygame.init()
-pygame.font.init()
 
 branco = (255,255,255)
 preto = (0,0,0)
@@ -18,27 +18,11 @@ relogio = pygame.time.Clock()
 fundo = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Snake')
 
-font = pygame.font.SysFont(None, 18)
-
-def text(msg, cor):
-
-    texto = font.render(msg, True, cor)
-    fundo.blit(texto, [largura/10, altura/5])
-
-def snake(cobra_xy):
-
-    for xy in cobra_xy:
-
-        pygame.draw.rect(fundo, branco, [xy[0], xy[1], tamanho, tamanho])
-
-def fruit(pos_x, pos_y):
-
-    pygame.draw.rect(fundo, vermelho, [pos_x, pos_y, tamanho, tamanho])
-
 pos_x = randrange(0, largura - tamanho, 10)
 pos_y = randrange(0, altura - tamanho, 10)
 ma_x = randrange(0, largura - tamanho, 10)
 ma_y = randrange(0, altura - tamanho, 10)
+
 velocidade_x = 0
 velocidade_y = 0
 cobra_xy = []
@@ -46,34 +30,29 @@ tam_cobra = 1
 
 game_over = False
 continua = True
+
 while continua:
-
     while game_over:
-
         fundo.fill(preto)
         text('Game over, tecle E para sair ou C para continuar', branco)
         pygame.display.update()
 
         for eventos in pygame.event.get():
-
             if eventos.type == pygame.QUIT:
-
                 continua = False
                 game_over = False
 
             if eventos.type == pygame.KEYDOWN:
-
                 if eventos.key == pygame.K_e:
-
                     continua = False
                     game_over = False
 
                 if eventos.key == pygame.K_c:
-
                     pos_x = randrange(0, largura - tamanho, 10)
                     pos_y = randrange(0, altura - tamanho, 10)
                     ma_x = randrange(0, largura - tamanho, 10)
                     ma_y = randrange(0, altura - tamanho, 10)
+
                     velocidade_x = 0
                     velocidade_y = 0
                     cobra_xy = []
@@ -82,36 +61,7 @@ while continua:
                     game_over = False
                     continua = True
 
-    for eventos in pygame.event.get():
-
-        if eventos.type == pygame.QUIT:
-
-            continua = False
-
-        if eventos.type == pygame.KEYDOWN:
-
-            if eventos.key == pygame.K_LEFT and velocidade_x != tamanho:
-
-                velocidade_y = 0
-                velocidade_x = - tamanho
-
-            if eventos.key == pygame.K_RIGHT and velocidade_x != -tamanho:
-
-                velocidade_y = 0
-                velocidade_x = tamanho
-
-            if eventos.key == pygame.K_DOWN and velocidade_y != - tamanho:
-
-                velocidade_x = 0
-                velocidade_y = tamanho
-
-            if eventos.key == pygame.K_UP and velocidade_y != tamanho:
-
-                velocidade_x = 0
-                velocidade_y = - tamanho
-
     if continua:
-
         fundo.fill(preto)
         cobra_inicio = []
         cobra_inicio.append(pos_x)
@@ -120,15 +70,12 @@ while continua:
         snake(cobra_xy)
 
         if len(cobra_xy) == tam_cobra:
-
             del cobra_xy[0]
 
         if any(bloco == cobra_inicio for bloco in cobra_xy[:-1]):
-
             game_over = True
 
         if pos_x == ma_x and pos_y == ma_y:
-
             ma_x = randrange(0, largura - tamanho, 10)
             ma_y = randrange(0, altura - tamanho, 10)
             tam_cobra += 1
@@ -140,19 +87,36 @@ while continua:
         relogio.tick(7)
 
         if pos_x >= largura:
-
             pos_x = 0
 
         if pos_x < 0:
-
             pos_x = largura - tamanho
 
         if pos_y >= altura:
-
             pos_y = 0
 
         if pos_y < 0:
-
             pos_y = altura - tamanho
+
+    for eventos in pygame.event.get():
+        if eventos.type == pygame.QUIT:
+            continua = False
+
+        if eventos.type == pygame.KEYDOWN:
+            if eventos.key == pygame.K_LEFT and velocidade_x != tamanho:
+                velocidade_y = 0
+                velocidade_x = - tamanho
+
+            if eventos.key == pygame.K_RIGHT and velocidade_x != -tamanho:
+                velocidade_y = 0
+                velocidade_x = tamanho
+
+            if eventos.key == pygame.K_DOWN and velocidade_y != - tamanho:
+                velocidade_x = 0
+                velocidade_y = tamanho
+
+            if eventos.key == pygame.K_UP and velocidade_y != tamanho:
+                velocidade_x = 0
+                velocidade_y = - tamanho
 
 pygame.quit()
